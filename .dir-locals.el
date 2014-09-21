1d3c205552b9cba807fe7056c6e6bd55a52f03db
Jeff Arnold <jeffrey.arnold@gmail.com>
2014-09-21 14:06:46 -0700

    fixed emacs time-stamps

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((markdown-mode . ((time-stamp-pattern . "-8/.. date: %:y-%:m-%:d %:H:%:M:%:S$")))
- (rst-mode . ((time-stamp-pattern . "-8/.. date: %:y-%:m-%:d %:H:%:M:%:S$")))
+((markdown-mode . ((time-stamp-pattern . "8/.. date: %:y-%02m-%02d %02H:%02M:%02S$")))
+ (rst-mode . ((time-stamp-pattern . "8/.. date: %:y-%02m-%02d %02H:%02M:%02S$")))
  )
