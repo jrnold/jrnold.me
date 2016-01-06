84f8ed9cd58c5be60b522452a24c74e731fc6404
jrnold <jeffrey.arnold@gmail.com>
2016-01-05 18:41:57 -0800

    add spell-check and link checks to site deploy

---
+++ spell-check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+rc=0
+find output -name "*.html" |
+    while read filename; do
+        misspellings=$(hunspell -d en_US,en_pols501 --check-apostrophe -H -l "$filename" 2>&1)
+        if [ ! -z "$misspellings" ]
+        then
+            echo "ERROR: mispelled words in $filename"
+            echo $misspellings | sort | uniq
+            rc=$(($rc + 1))
+        fi
+    done
+exit 0
+
